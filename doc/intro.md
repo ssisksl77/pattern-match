8f9f76968e75bc78a3c1fb371ad7618bc928895e
ssisksl77 <ssiskl77@gmail.com>
2022-10-20 23:15:46 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to yhnam/pattern-match
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/yhnam/pattern_match_test.clj
@@ -0,0 +1,7 @@
+(ns yhnam.pattern-match-test
+  (:require [clojure.test :refer :all]
+            [yhnam.pattern-match :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
